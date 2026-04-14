8bef931cadbd6deb9807e864f77426e384a82a33
未雨屏 <wyp999200637@qq.com>
2026-04-14 19:38:36 +0800

    默认使菜单从1开始

--- src/main/kotlin/cc/trixey/invero/core/panel/PanelPaged.kt
+++ src/main/kotlin/cc/trixey/invero/core/panel/PanelPaged.kt
@@ -34,19 +34,23 @@ class PanelPaged(
     override val locate: Pos?,
     @SerialName("default-page")
     @JsonNames("default", "def-page")
-    val defaultPage: Int = 0,
+    val defaultPage: Int = 1,
     @Serializable(with = ListAgentPanelSerializer::class)
     val pages: List<AgentPanel>
 ) : AgentPanel() {
 
     override val layout: Layout? = null
 
+    private val defaultPageIndex: Int = if (defaultPage <= 0) 0 else defaultPage - 1
+
     init {
-        require(defaultPage in pages.indices) { "Default Page ($defaultPage) is out of indices ${pages.indices}" }
+        require(defaultPageIndex in pages.indices) {
+            "Default Page ($defaultPage) is out of page range 1..${pages.size}"
+        }
     }
 
     override fun invoke(parent: PanelContainer, session: Session) =
-        parent.pagedNetesed(scale.pair, parent.locate(), defaultPage = defaultPage) {
+        parent.pagedNetesed(scale.pair, parent.locate(), defaultPage = defaultPageIndex) {
             pages.map { it.invoke(this, session) }
 
             // 临时补丁：页码变量初始化
