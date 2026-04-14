98b788120b4a6661517f91825314ee925d248453
未雨屏 <wyp999200637@qq.com>
2026-04-14 19:37:18 +0800

    适配CraftEngine26.4快照

--- src/main/kotlin/cc/trixey/invero/core/compat/item/CraftEngineItemProvider.kt
+++ src/main/kotlin/cc/trixey/invero/core/compat/item/CraftEngineItemProvider.kt
@@ -4,8 +4,8 @@ import cc.trixey.invero.common.ItemSourceProvider
 import cc.trixey.invero.core.Context
 import cc.trixey.invero.core.compat.DefItemProvider
 import cc.trixey.invero.core.compat.PluginHook
+import net.momirealms.craftengine.bukkit.api.BukkitAdaptor
 import net.momirealms.craftengine.bukkit.api.CraftEngineItems
-import net.momirealms.craftengine.bukkit.plugin.BukkitCraftEngine
 import net.momirealms.craftengine.core.util.Key
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
@@ -23,10 +23,10 @@ class CraftEngineItemProvider : ItemSourceProvider, PluginHook() {
         return try {
             val player = (context as? Context)?.viewer?.get<Player>() ?: return null
             val (namespace, id) = identifier.split(":", limit = 2)
-            val key = Key(namespace, id)
+            val key = Key(namespace,id)
             val item = CraftEngineItems.byId(key) ?: return null
-            val craftPlayer = BukkitCraftEngine.instance().adapt(player)
-            item.buildItemStack(craftPlayer)
+            val craftPlayer = BukkitAdaptor.adapt(player)?: return null
+            item.buildBukkitItem(craftPlayer)
         } catch (e: Exception) {
             // 如果出现异常（比如 CraftEngine 未安装），返回 null，让系统使用默认纹理
             null
